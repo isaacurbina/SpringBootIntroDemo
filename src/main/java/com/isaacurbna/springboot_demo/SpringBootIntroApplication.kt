d1f85920e00b788010f3daecc6ddce92d2075031
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-07-07 14:23:13 -0500

    first commit

---
+++ src/main/java/com/isaacurbna/springboot_demo/SpringBootIntroApplication.kt
@@ -0,0 +1,11 @@
+package com.isaacurbna.springboot_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootIntroApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootIntroApplication>(*args)
+}
